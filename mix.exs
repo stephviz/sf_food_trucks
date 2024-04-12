cffad6fc1f2204b4e5dd09b435041197a852e9e3
Stephanie Vizzi <stephanievizzi@gmail.com>
2024-04-12 05:24:06 -0400

    React home page
    home.html.heex
    * set react root element
    
    react-app.js
    * create root from id of root element
    
    app.js
    * call render fn on root element
    
    App.jsx
    * main react home page
    
    Map.jsx
    * map component via google-maps-react-markers
    
    app.html.heex
    * remove boilderplate
    
    app.css
    * map and app general styling
    
    package.lock, package.json
    * google-maps-react-markers
    
    mix.ex, mix.lock, endpoint.ex
    * corsica

--- mix.exs
+++ mix.exs
@@ -58,7 +58,8 @@ defmodule SFFoodTrucks.MixProject do
       {:jason, "~> 1.2"},
       {:dns_cluster, "~> 0.1.1"},
       {:bandit, "~> 1.2"},
-      {:nimble_csv, "~> 1.1"}
+      {:nimble_csv, "~> 1.1"},
+      {:corsica, "~> 2.0"}
     ]
   end
 
